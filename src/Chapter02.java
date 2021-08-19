7c23a597e1120b76e94a99b7d6d34c20f5a3000e
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-08-19 10:04:59 +0900

    テスト

--- src/Chapter02.java
+++ src/Chapter02.java
@@ -1,6 +1,7 @@
 
 public class Chapter02 {
     public static void main(String[] args) {
-        System.out.println("Ç±ÇÒÇ…ÇøÇÕÅI");
+        System.out.println("Hello WEBCAMPÅI");
+        System.out.println("Hello Java!");
     }
 }
