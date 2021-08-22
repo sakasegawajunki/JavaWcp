ba712cb652a2d949212644b540fbb25c29d84e88
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-08-22 22:26:44 +0900

    4章　確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int number1 = 777;
+		String name1 = "yumi";
+		System.out.println(name1);
+		
+		name1 = "suzuki";
+		System.out.println(name1);
+		
+		System.out.println(number1);
+				
+		
+	} 
+
+}
