db40b0993e66e538229a5fe9840f3e1ae6701899
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-08-22 23:09:23 +0900

    5章

---
+++ src/Null01.java
@@ -0,0 +1,14 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		string1 = "‚ ‚¢‚¤";
+		System.out.println(string1 + "‚Ì•¶š”F" + string1.length());
+		string1 = "";
+		System.out.println(string1 + "‚Ì•¶š”F" + string1.length());
+		string1 = null;
+		System.out.println(string1 + "‚Ì•¶š”F" + string1.length());
+		
+	}
+
+}
