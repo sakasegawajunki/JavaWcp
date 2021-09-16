81621c7c5be68f780711dfca287c95c889a16c07
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-09-16 16:35:31 +0900

    1章

---
+++ 応用編/Human.java
@@ -0,0 +1,11 @@
+public class Human {
+
+  public class HumanLeg {
+    public void kick() {
+      System.out.println("R‚é");
+      Human human = new Human();
+      Human.HumanLeg leg = human.new HumanLeg();
+      leg.kick();
+    }
+  }
+}
---
+++ 応用編/Outer.java
@@ -0,0 +1,9 @@
+
+public class Outer {
+	private String greeting = "‚±‚ñ‚É‚¿‚Í";
+	public class Inner {
+		public String hello() {
+			return greeting;
+		}
+	}
+}
